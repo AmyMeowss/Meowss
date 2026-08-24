35c192932cb3eefe11ba01013dae7e523a93e83d
AmyMeowss <282190644+AmyMeowss@users.noreply.github.com>
2026-08-24 17:45:48 +0200

    Example index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,13 @@
 @extends('layouts.app')
+
+@section('title', 'Feed')
+
+@section('content')
+    <div class="container">
+        <h1>{{ config('app.name') }}</h1>
+        <p>
+            {{-- TODO: Add text --}}
+            <b>{{ config('app.name') }}</b> is a microblogging platform.
+        </p>
+    </div>
+@endsection
